3cbba58ee731ba93e31f38e3ffd89553713392ff
Josh Pollock <jopo@mit.edu>
2021-08-14 11:56:49 -0400

    inline transforms at leaves for DOM readability

--- src/render.tsx
+++ src/render.tsx
@@ -5,14 +5,37 @@ const renderAux = (index: number, name: string, { bboxValues, encoding }: Compil
   console.log("name", name);
   console.log("bboxValues", bboxValues);
   console.log("encoding", encoding);
-  return (<g key={index} transform={`translate(${bboxValues.bbox.left} ${bboxValues.bbox.top})`}>
-    {Object.keys(encoding.children).map((glyphKey: any, index: number) =>
-      renderAux(index, glyphKey, { bboxValues: bboxValues.children[glyphKey], encoding: encoding.children[glyphKey] })
-    )}
-    {
-      encoding.renderFn !== undefined ? (encoding.renderFn as ((canvas: BBoxValues) => JSX.Element))(bboxValues.canvas) : <></>
-    }
-  </g>)
+
+  const childKeys = Object.keys(encoding.children);
+
+  if (childKeys.length == 0) {
+    // inline transforms at leaves for DOM readability
+    const left = bboxValues.bbox.left;
+    const top = bboxValues.bbox.top;
+    const canvas = bboxValues.canvas;
+    const values = {
+      left: canvas.left + left,
+      right: canvas.right + left,
+      top: canvas.top + top,
+      bottom: canvas.bottom + top,
+      width: canvas.width,
+      height: canvas.height,
+      centerX: canvas.centerX + left,
+      centerY: canvas.centerY + top,
+    };
+
+    return encoding.renderFn !== undefined ? (encoding.renderFn as ((canvas: BBoxValues) => JSX.Element))(values) : <></>;
+  } else {
+    return (<g key={index} transform={`translate(${bboxValues.bbox.left} ${bboxValues.bbox.top})`}>
+      {Object.keys(encoding.children).map((glyphKey: any, index: number) => (
+        renderAux(index, glyphKey, { bboxValues: bboxValues.children[glyphKey], encoding: encoding.children[glyphKey] })
+      )
+      )}
+      {
+        encoding.renderFn !== undefined ? (encoding.renderFn as ((canvas: BBoxValues) => JSX.Element))(bboxValues.canvas) : <></>
+      }
+    </g>)
+  }
 }
 
 export default ({ bboxValues, encoding }: CompiledAST): JSX.Element => {
