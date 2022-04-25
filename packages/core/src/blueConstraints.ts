697e69f87969a742fc0c004e494a5ba729b01eab
Josh Pollock <jopo@mit.edu>
2022-04-25 18:06:52 -0400

    comment out more blue stuff

--- packages/core/src/blueConstraints.ts
+++ packages/core/src/blueConstraints.ts
@@ -1,337 +1,339 @@
-// import { Constraint, mkAffineConstraint, plus, Variable, mkVar, strong } from 'indigo-ts';
-import { blue } from 'indigo-ts';
-import { bboxVarExprs } from './blueBBoxTransform';
+// // import { Constraint, mkAffineConstraint, plus, Variable, mkVar, strong } from 'indigo-ts';
+// import { blue } from 'indigo-ts';
+// import { bboxVarExprs } from './blueBBoxTransform';
 
-type Constraint = blue.Constraint;
-const mkAffineConstraint = blue.mkAffineConstraint;
-const plus = blue.plus;
-type Variable = blue.Variable;
-const mkVar = (s: string) => s;
+// type Constraint = blue.Constraint;
+// const mkAffineConstraint = blue.mkAffineConstraint;
+// const plus = blue.plus;
+// type Variable = blue.Variable;
+// const mkVar = (s: string) => s;
 
-const Strength = {
-  strong: blue.strong,
-}
-
-export type Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => Constraint;
-
-export const alignTop: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.top,
-    right.top
-  )
-}
-
-export const alignTopSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.top,
-    plus(right.top, spacing),
-    strength,
-  );
-}
-
-export const alignRightSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.right,
-    plus(right.right, spacing),
-    strength,
-  );
-}
-
-export const alignBottomSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    plus(left.bottom, spacing),
-    right.bottom,
-    strength,
-  );
-}
-
-export const alignLeftSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    plus(left.left, spacing),
-    right.left,
-    strength,
-  );
-}
-
-export const alignBottom: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.bottom,
-    right.bottom
-  )
-}
-
-export const alignLeft: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.left,
-    right.left
-  )
-}
-
-export const alignRight: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.right,
-    right.right
-  )
-}
-
-export const alignCenterX: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.centerX,
-    right.centerX
-  )
-}
-
-export const alignCenterY: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.centerY,
-    right.centerY
-  )
-}
-
-export const alignCenterXStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.centerX,
-    right.centerX,
-    Strength.strong,
-  )
-}
-
-export const alignCenterYStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.centerY,
-    right.centerY,
-    Strength.strong,
-  )
-}
-
-export const alignCenterXOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.centerX,
-    right.centerX,
-    strength,
-  )
-}
-
-export const alignCenterYOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.centerY,
-    right.centerY,
-    strength,
-  )
-}
-
-export const hAlignCenter = alignCenterY;
-export const vAlignCenter = alignCenterX;
-
-export const alignCenter = alignCenterX;
-export const alignMiddle = alignCenterY;
-
-export const hAlignCenterStrong = alignCenterYStrong;
-export const vAlignCenterStrong = alignCenterXStrong;
-
-export const alignCenterStrong = alignCenterXStrong;
-export const alignMiddleStrong = alignCenterYStrong;
-
-export const hAlignCenterOptions = alignCenterYOptions;
-export const vAlignCenterOptions = alignCenterXOptions;
-
-export const alignCenterOptions = alignCenterXOptions;
-export const alignMiddleOptions = alignCenterYOptions;
-
-export const hSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    plus(left.right, spacing),
-    right.left,
-    strength,
-  );
-}
-
-export const vSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    plus(left.bottom, spacing),
-    right.top,
-    strength,
-  );
-}
-
-// export const containsLeft = (left: bboxVarExprs, right: bboxVarExprs) => {
-//   return mkAffineConstraint(
-//     left.left,
-//     'le',
-//     right.left,
-//   )
+// const Strength = {
+//   strong: blue.strong,
 // }
 
-// export const containsRight = (left: bboxVarExprs, right: bboxVarExprs) => {
-//   return mkAffineConstraint(
-//     left.right,
-//     'ge',
-//     right.right,
-//   )
-// }
+// export type Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => Constraint;
 
-// export const containsTop = (left: bboxVarExprs, right: bboxVarExprs) => {
+// export const alignTop: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
 //   return mkAffineConstraint(
 //     left.top,
-//     'le',
-//     right.top,
+//     right.top
 //   )
 // }
 
-// export const containsBottom = (left: bboxVarExprs, right: bboxVarExprs) => {
+// export const alignTopSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.top,
+//     plus(right.top, spacing),
+//     strength,
+//   );
+// }
+
+// export const alignRightSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.right,
+//     plus(right.right, spacing),
+//     strength,
+//   );
+// }
+
+// export const alignBottomSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     plus(left.bottom, spacing),
+//     right.bottom,
+//     strength,
+//   );
+// }
+
+// export const alignLeftSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     plus(left.left, spacing),
+//     right.left,
+//     strength,
+//   );
+// }
+
+// export const alignBottom: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
 //   return mkAffineConstraint(
 //     left.bottom,
-//     'ge',
-//     right.bottom,
+//     right.bottom
 //   )
 // }
 
-export const alignTopStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.top,
-    right.top,
-    Strength.strong,
-  )
-}
+// export const alignLeft: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.left,
+//     right.left
+//   )
+// }
 
-export const alignBottomStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.bottom,
-    right.bottom,
-    Strength.strong,
-  )
-}
+// export const alignRight: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.right,
+//     right.right
+//   )
+// }
 
-export const alignLeftStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.left,
-    right.left,
-    Strength.strong,
-  )
-}
+// export const alignCenterX: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.centerX,
+//     right.centerX
+//   )
+// }
 
-export const alignRightStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.right,
-    right.right,
-    Strength.strong,
-  )
-}
+// export const alignCenterY: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.centerY,
+//     right.centerY
+//   )
+// }
 
-export const alignTopOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.top,
-    right.top,
-    strength,
-  )
-}
+// export const alignCenterXStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.centerX,
+//     right.centerX,
+//     Strength.strong,
+//   )
+// }
 
-export const alignBottomOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.bottom,
-    right.bottom,
-    strength,
-  )
-}
+// export const alignCenterYStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.centerY,
+//     right.centerY,
+//     Strength.strong,
+//   )
+// }
 
-export const alignLeftOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.left,
-    right.left,
-    strength,
-  )
-}
+// export const alignCenterXOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.centerX,
+//     right.centerX,
+//     strength,
+//   )
+// }
 
-export const alignRightOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.right,
-    right.right,
-    strength,
-  )
-}
+// export const alignCenterYOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.centerY,
+//     right.centerY,
+//     strength,
+//   )
+// }
 
-export const sameWidth: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.width,
-    right.width
-  )
-}
+// export const hAlignCenter = alignCenterY;
+// export const vAlignCenter = alignCenterX;
 
-export const sameHeight: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.height,
-    right.height
-  )
-}
+// export const alignCenter = alignCenterX;
+// export const alignMiddle = alignCenterY;
 
-export const sameWidthStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.width,
-    right.width,
-    Strength.strong,
-  )
-}
+// export const hAlignCenterStrong = alignCenterYStrong;
+// export const vAlignCenterStrong = alignCenterXStrong;
 
-export const sameHeightStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.height,
-    right.height,
-    Strength.strong,
-  )
-}
+// export const alignCenterStrong = alignCenterXStrong;
+// export const alignMiddleStrong = alignCenterYStrong;
 
-export const sameWidthOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.width,
-    right.width,
-    strength,
-  )
-}
+// export const hAlignCenterOptions = alignCenterYOptions;
+// export const vAlignCenterOptions = alignCenterXOptions;
 
-export const sameHeightOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.height,
-    right.height,
-    strength,
-  )
-}
+// export const alignCenterOptions = alignCenterXOptions;
+// export const alignMiddleOptions = alignCenterYOptions;
 
-export const sameSize: Gestalt[] = [sameWidth, sameHeight];
-export const sameBox: Gestalt[] = [...sameSize, alignCenter, alignMiddle];
+// export const hSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     plus(left.right, spacing),
+//     right.left,
+//     strength,
+//   );
+// }
 
-// export const contains: Gestalt[] = [containsLeft, containsRight, containsTop, containsBottom];
+// export const vSpace = (spacing: number | Variable = mkVar(''), strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     plus(left.bottom, spacing),
+//     right.top,
+//     strength,
+//   );
+// }
 
-// export const containsShrinkWrap: Gestalt[] = [...contains, alignLeftStrong, alignRightStrong, alignTopStrong, alignBottomStrong];
+// // export const containsLeft = (left: bboxVarExprs, right: bboxVarExprs) => {
+// //   return mkAffineConstraint(
+// //     left.left,
+// //     'le',
+// //     right.left,
+// //   )
+// // }
 
-// used for circles
-export const eqWidthHeight = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.width,
-    right.height,
-  )
-}
+// // export const containsRight = (left: bboxVarExprs, right: bboxVarExprs) => {
+// //   return mkAffineConstraint(
+// //     left.right,
+// //     'ge',
+// //     right.right,
+// //   )
+// // }
 
-export const makeEqual = (leftDim: keyof bboxVarExprs, rightDim: keyof bboxVarExprs, strength?: number) =>
-  (left: bboxVarExprs, right: bboxVarExprs) => {
-    return mkAffineConstraint(
-      left[leftDim],
-      right[rightDim],
-      strength,
-    )
-  }
+// // export const containsTop = (left: bboxVarExprs, right: bboxVarExprs) => {
+// //   return mkAffineConstraint(
+// //     left.top,
+// //     'le',
+// //     right.top,
+// //   )
+// // }
 
-export const eqWidthHeightStrong = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.width,
-    right.height,
-    Strength.strong,
-  )
-}
+// // export const containsBottom = (left: bboxVarExprs, right: bboxVarExprs) => {
+// //   return mkAffineConstraint(
+// //     left.bottom,
+// //     'ge',
+// //     right.bottom,
+// //   )
+// // }
 
-export const eqWidthHeightOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.width,
-    right.height,
-    Strength.strong,
-  )
-}
+// export const alignTopStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.top,
+//     right.top,
+//     Strength.strong,
+//   )
+// }
+
+// export const alignBottomStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.bottom,
+//     right.bottom,
+//     Strength.strong,
+//   )
+// }
+
+// export const alignLeftStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.left,
+//     right.left,
+//     Strength.strong,
+//   )
+// }
+
+// export const alignRightStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.right,
+//     right.right,
+//     Strength.strong,
+//   )
+// }
+
+// export const alignTopOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.top,
+//     right.top,
+//     strength,
+//   )
+// }
+
+// export const alignBottomOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.bottom,
+//     right.bottom,
+//     strength,
+//   )
+// }
+
+// export const alignLeftOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.left,
+//     right.left,
+//     strength,
+//   )
+// }
+
+// export const alignRightOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.right,
+//     right.right,
+//     strength,
+//   )
+// }
+
+// export const sameWidth: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.width,
+//     right.width
+//   )
+// }
+
+// export const sameHeight: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.height,
+//     right.height
+//   )
+// }
+
+// export const sameWidthStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.width,
+//     right.width,
+//     Strength.strong,
+//   )
+// }
+
+// export const sameHeightStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.height,
+//     right.height,
+//     Strength.strong,
+//   )
+// }
+
+// export const sameWidthOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.width,
+//     right.width,
+//     strength,
+//   )
+// }
+
+// export const sameHeightOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.height,
+//     right.height,
+//     strength,
+//   )
+// }
+
+// export const sameSize: Gestalt[] = [sameWidth, sameHeight];
+// export const sameBox: Gestalt[] = [...sameSize, alignCenter, alignMiddle];
+
+// // export const contains: Gestalt[] = [containsLeft, containsRight, containsTop, containsBottom];
+
+// // export const containsShrinkWrap: Gestalt[] = [...contains, alignLeftStrong, alignRightStrong, alignTopStrong, alignBottomStrong];
+
+// // used for circles
+// export const eqWidthHeight = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.width,
+//     right.height,
+//   )
+// }
+
+// export const makeEqual = (leftDim: keyof bboxVarExprs, rightDim: keyof bboxVarExprs, strength?: number) =>
+//   (left: bboxVarExprs, right: bboxVarExprs) => {
+//     return mkAffineConstraint(
+//       left[leftDim],
+//       right[rightDim],
+//       strength,
+//     )
+//   }
+
+// export const eqWidthHeightStrong = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.width,
+//     right.height,
+//     Strength.strong,
+//   )
+// }
+
+// export const eqWidthHeightOptions = (strength?: number): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.width,
+//     right.height,
+//     Strength.strong,
+//   )
+// }
+
+export { }
--- packages/core/src/blueGestalt.ts
+++ packages/core/src/blueGestalt.ts
@@ -1,150 +1,152 @@
-import { bboxVarExprs } from './blueBBoxTransform';
-// import { Variable, Constraint, mkAffineConstraint, plus, mkVar, strong } from 'indigo-ts';
-import { blue } from 'indigo-ts';
-type Constraint = blue.Constraint;
-type Variable = blue.Variable;
-const mkAffineConstraint = blue.mkAffineConstraint;
-const plus = blue.plus;
-const strong = blue.strong;
-// TODO: figure out how to actually do the variable sharing stuff...
-const mkVar = (s: string) => s;
+// import { bboxVarExprs } from './blueBBoxTransform';
+// // import { Variable, Constraint, mkAffineConstraint, plus, mkVar, strong } from 'indigo-ts';
+// import { blue } from 'indigo-ts';
+// type Constraint = blue.Constraint;
+// type Variable = blue.Variable;
+// const mkAffineConstraint = blue.mkAffineConstraint;
+// const plus = blue.plus;
+// const strong = blue.strong;
+// // TODO: figure out how to actually do the variable sharing stuff...
+// const mkVar = (s: string) => s;
 
-export type Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => Constraint;
+// export type Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => Constraint;
 
-export const alignTop: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.top,
-    right.top
-  )
-}
+// export const alignTop: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.top,
+//     right.top
+//   )
+// }
 
-export const alignTopSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.top,
-    plus(right.top, spacing)
-  );
-}
+// export const alignTopSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.top,
+//     plus(right.top, spacing)
+//   );
+// }
 
-export const alignRightSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.right,
-    plus(right.right, spacing)
-  );
-}
+// export const alignRightSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.right,
+//     plus(right.right, spacing)
+//   );
+// }
 
-export const alignBottomSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    plus(left.bottom, spacing),
-    right.bottom,
-  );
-}
+// export const alignBottomSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     plus(left.bottom, spacing),
+//     right.bottom,
+//   );
+// }
 
-export const alignLeftSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    plus(left.left, spacing),
-    right.left,
-  );
-}
+// export const alignLeftSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     plus(left.left, spacing),
+//     right.left,
+//   );
+// }
 
-export const alignBottom: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.bottom,
-    right.bottom
-  )
-}
+// export const alignBottom: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.bottom,
+//     right.bottom
+//   )
+// }
 
-export const alignLeft: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.left,
-    right.left
-  )
-}
+// export const alignLeft: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.left,
+//     right.left
+//   )
+// }
 
-export const alignRight: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.right,
-    right.right
-  )
-}
+// export const alignRight: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.right,
+//     right.right
+//   )
+// }
 
-export const alignCenterX: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.centerX,
-    right.centerX
-  )
-}
+// export const alignCenterX: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.centerX,
+//     right.centerX
+//   )
+// }
 
-export const alignCenterY: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.centerY,
-    right.centerY
-  )
-}
+// export const alignCenterY: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.centerY,
+//     right.centerY
+//   )
+// }
 
-export const hAlignCenter = alignCenterY;
-export const vAlignCenter = alignCenterX;
+// export const hAlignCenter = alignCenterY;
+// export const vAlignCenter = alignCenterX;
 
-export const hSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    plus(left.right, spacing),
-    right.left,
-  );
-}
+// export const hSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     plus(left.right, spacing),
+//     right.left,
+//   );
+// }
 
-export const vSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    plus(left.bottom, spacing),
-    right.top,
-  );
-}
+// export const vSpace = (spacing: number | Variable = mkVar('')): Gestalt => (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     plus(left.bottom, spacing),
+//     right.top,
+//   );
+// }
 
-export const alignTopStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.top,
-    right.top,
-    strong,
-  )
-}
+// export const alignTopStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.top,
+//     right.top,
+//     strong,
+//   )
+// }
 
-export const alignBottomStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.bottom,
-    right.bottom,
-    strong,
-  )
-}
+// export const alignBottomStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.bottom,
+//     right.bottom,
+//     strong,
+//   )
+// }
 
-export const alignLeftStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.left,
-    right.left,
-    strong,
-  )
-}
+// export const alignLeftStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.left,
+//     right.left,
+//     strong,
+//   )
+// }
 
-export const alignRightStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.right,
-    right.right,
-    strong,
-  )
-}
+// export const alignRightStrong: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.right,
+//     right.right,
+//     strong,
+//   )
+// }
 
-export const sameWidth: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.width,
-    right.width
-  )
-}
+// export const sameWidth: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.width,
+//     right.width
+//   )
+// }
 
-export const sameHeight: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
-  return mkAffineConstraint(
-    left.height,
-    right.height
-  )
-}
+// export const sameHeight: Gestalt = (left: bboxVarExprs, right: bboxVarExprs) => {
+//   return mkAffineConstraint(
+//     left.height,
+//     right.height
+//   )
+// }
 
-// TODO: contains constraint should use max and min somehow...
-// export const contains: Gestalt[] = [containsLeft, containsRight, containsTop, containsBottom];
+// // TODO: contains constraint should use max and min somehow...
+// // export const contains: Gestalt[] = [containsLeft, containsRight, containsTop, containsBottom];
 
-// export const containsShrinkWrap: Gestalt[] = [...contains, alignLeftStrong, alignRightStrong, alignTopStrong, alignBottomStrong];
+// // export const containsShrinkWrap: Gestalt[] = [...contains, alignLeftStrong, alignRightStrong, alignTopStrong, alignBottomStrong];
+
+export { }
